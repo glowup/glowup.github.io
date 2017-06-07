925c51eb7d90dfbc8786f1c7caf7817bbbbb6797
otherwall <phillip.a.jordan@gmail.com>
2017-06-07 11:26:19 +0700

    fixed script links

--- standard.js
+++ standard.js
@@ -5,7 +5,7 @@ function inIframe () {
         return true;
     }
 }
-allfiles = ["intro.html", "html.html", "html/syntax.html", "html/elements.html", "html/structure.html", "css.html"];
+allfiles = ["intro/", "html/", "html/syntax.html", "html/elements.html", "html/structure.html", "css/"];
 if (inIframe()){
     allfilesindex = allfiles.indexOf(window.location.substr(window.location.indexOf(".com/")+5));
     if (allfilesindex){
